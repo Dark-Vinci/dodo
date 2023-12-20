ae23495911b12c0be9e2c7d7c00077dea1708c10
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-20 11:49:45 +0100

    chore: more update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,24 @@ import style from './Footer.module.scss';
 export function Footer(): JSX.Element {
     return (
         <div className={ style.container}>
-            <div className={ style.footer_container }>FOOTER</div>
+            <div className={ style.footer_container }>
+                <div className="top_container">
+                    <div className="top">
+                        <div className="span_container">
+                            <span>Just DODO it!</span>
+                            <span>Just DODO it!</span>
+                            <span>Just DODO it!</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="body_container">
+                    <div className="body">
+                        <div className="left_container">LEFT</div>
+                        <div className="right_container">RIGHT</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
