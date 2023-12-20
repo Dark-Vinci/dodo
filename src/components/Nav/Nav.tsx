c2f10e3684e81c9ca4b1ee8f81fe92b416979c5c
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-20 20:35:01 +0100

    chore: more update

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,7 +5,30 @@ import style from './Nav.module.scss';
 export function Nav(): JSX.Element {
     return (
         <div className={ style.container }>
-            <div className={ style.nav_container }>NAVIGATION</div>
+            <div className={ style.nav_container }>
+                <div className="left">
+                    <div className="left_container">
+                        <div className="logo"></div>
+                        <div className="tabs">
+                            <ul>
+                                <li>Products</li>
+                                <li>Governance</li>
+                                <li>Research</li>
+                                <li>Documentation</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="input">
+                        <div className="input_container">
+                            <span className="icon"></span>
+                            <input type="text" placeholder='Product name, Pool addr..'/>
+                        </div>
+                    </div>
+                    <div className="logo"></div>
+                </div>
+            </div>
         </div>
     );
 }
